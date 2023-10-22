0515013c4c4f19327cb41a0954a9088ef87c42ce
IgorPRibeiro <igor.ribeiro@globalsys.com.br>
2023-10-21 21:53:00 -0300

    Gerenciando estado e distribuindo .

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # pokemon_flutter
 
 A new Flutter project. Created for study state architecture.
+Usem os commits como referencia de estudo.
+
 
 # Separando o store em camadas / Gerenciando o estado
 
-- Criando uma classe apenas para o controle de estado ( `pokemon_store.dart` ).
-  - 1º forma:
+***representar o estado -> gerenciar o estado -> distribuir o estado***
+
+- 1º forma (representar os estado):
+  - Usando a propria tela para chamar o estados deixando tudo em arquivo apenas ;
+  
+- 2º forma (gerenciar o estado) :
+    - Criando um arquivo (`pokemon_store.dart`) para GERENCIAR o estado. Retirando da tela e organinzando o código.;
     - Chamando o `notifyListeners()` no controller (reatividade). Forma de informar a View que o estado foi atualizado;
     - Criar um `store.addListener()` na tela e executar o `setState` para atualizar o estado da tela;
+    
+- 3º forma (distribuir o estado):
+    - Usar um componente para assinar/monitorar(distribuir) essa reatividade;
+      - Dessa forma não será preciso a criação do `addListener()` na tela;
+    - Usando o `ListenableBuilder`, passando o nosso `store` para ser assinado/monitorado e o que será renderizado no builder 
+      - Com isso o Widget (componente) não precisará ser Stateful, podendo ser StateLess
   
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,61 +4,43 @@ import 'package:pokemon_flutter/models/pokemon.dart';
 import 'package:pokemon_flutter/pokemon_service.dart';
 import 'package:pokemon_flutter/pokemon_store.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   final store = PokemonStore();
-
-  // identify this widget as a listener
-  // indentificar esse widget(componente) como um ouvinte
-  @override
-  void initState() {
-    super.initState();
-    store.getPokemons();
-    store.addListener(() {
-      // every time that call a listener in store, will be executed the bellow function
-      // toda vez que chamar um listener no store, sera executara a func abaixo
-      setState(() {});
-      // calling the setState, state of screen will be actualized
-      // chamando o setState, ele ira atualizar o estado da tela
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    Widget body = Container();
-    if (store.isLoading) {
-      body = const Center(
-        child: CircularProgressIndicator(),
-      );
-    } else if (store.error.isNotEmpty) {
-      body = Center(
-        child: ElevatedButton(
-          onPressed: store.getPokemons,
-          child: Text(store.error),
-        ),
-      );
-    } else {
-      body = ListView.builder(
-          itemCount: store.pokemons.length,
-          itemBuilder: (context, index) {
-            final pokemon = store.pokemons[index];
-            return ListTile(
-              title: Text(pokemon.name),
-            );
-          });
-    }
+    return ListenableBuilder(listenable: store, builder: (context, child) {
+      Widget body = Container();
+      if (store.isLoading) {
+        body = const Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (store.error.isNotEmpty) {
+        body = Center(
+          child: ElevatedButton(
+            onPressed: store.getPokemons,
+            child: Text(store.error),
+          ),
+        );
+      } else {
+        body = ListView.builder(
+            itemCount: store.pokemons.length,
+            itemBuilder: (context, index) {
+              final pokemon = store.pokemons[index];
+              return ListTile(
+                title: Text(pokemon.name),
+              );
+            });
+      }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pokemon"),
-      ),
-      body: body,
-    );
-  }
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Pokemon"),
+        ),
+        body: body,
+      );
+    });
+
 }
